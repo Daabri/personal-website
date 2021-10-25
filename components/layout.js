5abca2cb5563db4c5db37f9bceb03308d0c76e77
Daan Brinkhuis <brinkhuis.daan@gmail.com>
2021-10-25 08:49:18 +0200

    Create sections

--- components/layout.js
+++ components/layout.js
@@ -8,59 +8,63 @@ export const siteTitle = 'Daan Brinkhuis'
 
 export default function Layout({ children, home }) {
 	return (
-		<div className={styles.container}>
-			<Head>
-				<link rel="icon" href="/favicon.ico" />
-				<meta
-					name="description"
-					content="content"
-				/>
-				<meta
-					property="og:image"
-					content={`https://og-image.now.sh/${encodeURI(
-						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-				/>
-				<meta name="og:title" content={siteTitle} />
-				<meta name="twitter:card" content="summary_large_image" />
-			</Head>
-			<header className={styles.header}>
-				{home ? (
-					<>
-						<img
-							src="/images/profile2.jpg"
-							className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-							alt={name}
-						/>
-						<h1 className={utilStyles.heading2Xl}>{name}</h1>
-					</>
-				) : (
-					<>
-						<Link href="/">
-							<a>
-								<img
-									src="/images/profile2.jpg"
-									className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-									alt={name}
-								/>
-							</a>
-						</Link>
-						<h2 className={utilStyles.headingLg}>
+		<>
+			<div className={styles.container}>
+				<Head>
+					<link rel="icon" href="/favicon.ico" />
+					<meta
+						name="description"
+						content="content"
+					/>
+					<meta
+						property="og:image"
+						content={`https://og-image.now.sh/${encodeURI(
+							siteTitle
+						)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+					/>
+					<meta name="og:title" content={siteTitle} />
+					<meta name="twitter:card" content="summary_large_image" />
+				</Head>
+				<header className={styles.header}>
+					{home ? (
+						<>
+							<img
+								src="/images/profile.jpg"
+								className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+								alt={name}
+							/>
+							<h1 className={utilStyles.heading2Xl}>{name}</h1>
+						</>
+					) : (
+						<>
 							<Link href="/">
-								<a className={utilStyles.colorInherit}>{name}</a>
+								<a>
+									<img
+										src="/images/profile.jpg"
+										className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+										alt={name}
+									/>
+								</a>
 							</Link>
-						</h2>
-					</>
+							<h2 className={utilStyles.headingLg}>
+								<Link href="/">
+									<a className={utilStyles.colorInherit}>{name}</a>
+								</Link>
+							</h2>
+						</>
+					)}
+				</header>
+			</div>
+			<div className={styles.container}>
+				<main>{children}</main>
+				{!home && (
+					<div className={styles.backToHome}>
+						<Link href="/">
+							<a>← Back to home</a>
+						</Link>
+					</div>
 				)}
-			</header>
-			<main>{children}</main>
-			{!home && (
-				<div className={styles.backToHome}>
-					<Link href="/">
-						<a>← Back to home</a>
-					</Link>
-				</div>
-			)}
-		</div>
+			</div>
+		</>
 	)
 }
